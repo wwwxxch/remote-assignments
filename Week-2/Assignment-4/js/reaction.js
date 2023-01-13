f2fcfa8b65ddf28d10c228320a0ce68fdea29227
Chih-Hui Wang <chihhui.x@gmail.com>
2023-01-13 12:54:27 +0800

    update js for clicking function

--- Week-2/Assignment-4/js/reaction.js
+++ Week-2/Assignment-4/js/reaction.js
@@ -1,7 +1,19 @@
-let chgtext = document.getElementById("welcome");
+// Change text by clicking
+const chgtext = document.getElementById("welcome");
 chgtext.addEventListener("click", () => {
-  chgtext.innerHTML = "Have a Good Time!";
+  chgtext.textContent = "Have a Good Time!";
 });
 
-
+// Show hidden boxes
+const btnClick = document.querySelector('.button');
+btnClick.addEventListener('click', () => {
+  const hiddenBox = document.querySelectorAll('.hidegrandparent');
+  for (let i = 0; i < hiddenBox.length; i++) {
+    if (hiddenBox[i].style.display === "" || hiddenBox[i].style.display === "none") {
+      hiddenBox[i].style.display = 'flex';
+    } else {
+      hiddenBox[i].style.display = 'none';
+    }
+  }  
+})
 
